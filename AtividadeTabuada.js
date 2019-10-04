fe0cd3aeb840dd5d18cb6c182708545af07417d3
Larissa de Almeida Ribeiro <1901025@SALAS.AULAS>
2019-10-03 21:58:36 -0300

    Aula de JavaScrip - Atividade de java.

---
+++ AtividadeTabuada.js
@@ -0,0 +1,29 @@
+
+//meu Código
+function tabuada(){
+    var cont= 1;
+    
+    while(cont <=10){
+        tabuada = 5 * cont
+        document.write("5 x " + cont + " =  " + tabuada + "<br/>");
+        cont++;
+     }
+ }
+
+ 
+/*
+// Feito profesor!
+ function tabuada(){
+    var cont= 1;
+    while(cont <=10){
+        document.write("5 x " +cont+ " = " +cont+ "<br/>");
+        cont++;
+     }
+ }
+*/
+
+
+ 
+
+ 
+
---
+++ java.js
@@ -0,0 +1,75 @@
+// Criação de variável...
+/*var nome = "";
+var sobrenome = "";
+
+var numero = 1; 
+
+nome= "Mônica";
+sobrenome="Dente";
+
+var nome_complemento = nome + sobrenome;
+alert(nome_complemento);
+ || -> ou 
+
+if( 2+3==5 || 8*3<1){
+    alert("Verdadeiro");
+}
+else{
+    alert("Mentirinha");
+}
+
+if(5>10){
+    alert("Falso.");
+}
+else{
+    alert("Estou no else");
+}
+*/
+
+/*
+do{
+
+}
+while(cont >5){
+
+}
+*/
+
+/*var cont= 1; 
+while(cont <=5){
+    alert(cont);
+    cont= cont +1;
+}
+var cont= 5; 
+while(cont >=0){
+    alert(cont);
+    cont--;
+}
+
+// Estrutura do for 
+for(var i=0; 1<=5; i++ ){
+    alert(i);
+}
+*/
+/* 
+ --> Função com parametro.
+
+ function somar(n1,n2){
+     return n1+n2;
+     
+ }
+ //var resultado = somar(5,3);
+ //alert(resultado);  
+ //alert(somar(5,10));
+
+ document.write(somar(5,3));*/
+
+ function somar(){
+    
+     var cont= 1;
+     while(cont <=5){
+        document.write(cont + "<br/>");
+        cont++;
+     }
+ }
+ //
