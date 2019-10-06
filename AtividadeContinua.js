f3ee5ffe22707956427c241c500ea067a3e4235c
lalmeidaribeiro <rlarissa191@gmail.com>
2019-10-06 12:05:43 -0300

    Resolvendo a Atividade Contínua06

---
+++ AtividadeContinua.js
@@ -0,0 +1,11 @@
+// Questão 01
+ /*function somar(temp,ind){
+    a =(temp == ind);
+    b =(temp === ind);
+
+   return b;  
+}
+var  a,b= null
+var resultado = somar(null,2);
+alert(resultado); 
+*/
